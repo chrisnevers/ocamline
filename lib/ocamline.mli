3f7e2ff9fdfed97e0084945d532de5af25d306cf
Chris Nevers <cnevers@cainc.com>
2019-11-06 17:21:18 -0500

    Update type signature

--- lib/ocamline.mli
+++ lib/ocamline.mli
@@ -1,3 +1,20 @@
+(**
+  [read ?trim_delim ?brackets ?prompt ?strings delim] will read input from
+  stdin until a new line or [delim] string is encountered. Occurrences of
+  [delim] not at the end of the line will not stop the input process.
+  If [delim] is an empty string, it will return on new lines.
+
+  [trim_delim]: Whether or not to remove the line delimiter from the return value.
+
+  [brackets]: Any characters that once opened must be closed before the line is accepted.
+
+  [prompt]: Customizes the prompt displayed to the user.
+
+  [strings]: If brackets are in strings, they won't have to be closed.
+
+  [delim]: The string that, when found, halts scanning and returns the input.
+
+  *)
 val read :
   ?trim_delim:bool ->
   ?brackets:(char * char) list ->
@@ -5,9 +22,3 @@ val read :
   ?strings:char list ->
   string ->
   string
-
-(* Helpers *)
-
-val _ends_with : string -> string -> int -> bool
-
-val _count_char : char -> char list -> char list -> int
